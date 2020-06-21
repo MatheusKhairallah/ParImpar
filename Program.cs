c565b7aeaa6a919bca00b78f1595ed01ee6d205a
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:49:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            double numero=Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            if(numero%2==0)
+            Console.WriteLine($"{numero} é par");
+            else
+            Console.WriteLine($"{numero} é ímpar");
+        }
+    }
+}
